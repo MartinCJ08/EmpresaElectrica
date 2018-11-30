5469fb732320dd5a6689c55fc1b3aa7c57a8215e
MartinCJ08 <martin.carrascoj@gmail.com>
2018-11-29 23:28:48 -0700

    Signed-off-by: MartinCJ08 <martin.carrascoj@gmail.com>

--- README.md
+++
@@ -1,2 +0,0 @@
-# EmpresaElectrica
-Una página web que integra distintas aplicaciones administrativas y financieras. Vato.
--- updateMaterial.php
+++ updateMaterial.php
@@ -9,11 +9,11 @@
   <li><a class="active" href="index.php">Principal</a></li>
   <li><a href="clienteAdmon.php">Clientes</a></li>
   <li><a href="empleadoAdmon.php">Empleados</a></li>
-  <li><a href="#cotizacionAdmon.php">Cotizaciones</a></li>
-  <li><a href="#trabajoAdmon.php">Trabajos</a></li>
-  <li><a href="#materialAdmon.php">Material</a></li>
-  <li><a href="#equipoAdmon.php">Equipo</a></li>
-  <li><a href="#prospectoAdmon.php">Prospecto</a></li>
+  <li><a href="cotizacionAdmon.php">Cotizaciones</a></li>
+  <li><a href="trabajoAdmon.php">Trabajos</a></li>
+  <li><a href="materialAdmon.php">Material</a></li>
+  <li><a href="equipoAdmon.php">Equipo</a></li>
+  <li><a href="prospectoAdmon.php">Prospecto</a></li>
 </ul>
 
 <div style="padding:20px;margin-top:30px;height:1500px;">
@@ -22,6 +22,8 @@
 idMaterial:
  <input type="text" name="idc" size="5" maxlength="10">
  <input class="buttonSha" type="submit" name="accion" value="Buscar">
+
+<a class="buttonSha" href="materialAdmon.php">Regresar</a>
 </FORM>
 </div>
 </body>
